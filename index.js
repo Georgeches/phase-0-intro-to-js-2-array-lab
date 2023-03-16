04bea6976eabadd280e775e2bf499e10dd6cf446
Georgeches <georgechesire83@gmail.com>
2023-03-16 11:36:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.append("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.prepend("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(4, 4)
+}
+
+function removeFirstCat(){
+    let new_arr = cats.splice(0,1)
+}
